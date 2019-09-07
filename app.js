bb2f26ddba5c8797899fe90a95464f2db921f025
jitu <jitu1612@gmail.com>
2019-09-07 16:46:37 +0530

    v1.0.13

--- app.js
+++ app.js
@@ -2,70 +2,85 @@ const cheerio=require('cheerio');
 const request = require('request');
 var args = process.argv;
 
-//var url="https://grofers.com/prn/maggi-masala-noodles/prid/169812"
+//node app.js -u https://www.naturesbasket.co.in/Products/Taaza---Homogenised-Toned-Milk---Amul/200_0_0_0
 //node app.js -u https://grofers.com/prn/maggi-masala-noodles/prid/169812
 //node app.js -u https://www.bigbasket.com/pd/100006283/premier-special-face-tissue-100-pcs/
 
 
 var scrapeGBN= function(url,callback){
-
+    
     if(!url){
+        msg("Please give URL")
         callback && callback("Please give URL",null)
-    }
-
-    var options = {
-        headers: {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36'},
-        method: 'GET'
-    }
-request(url, options, function(err, response, html){
-    if(!err){
-        var $= cheerio.load(html);
-        var json={}
-        if(url.match("grofers.com")){
-            var name= $('.pdp-product__name').text()
-            var desc=$('.product-attribute__attr--description').text()
-            var price=$('.pdp-product__price--new').text()
-            var size=$('.product-variant__btn--active').text()
-              
-            json['name']=name
-            json['description']=desc
-            json['price']=price
-            json['size']=size
-            msg(json)
-            callback && callback(null,json)
-        }else if(url.match("bigbasket.com")){
-            var name= $('h1').text()
-            var desc=$('._26MFu').children().eq(1).find('p').text()
-            var price=$('.IyLvo').text()
-            var size=$('._3Yybm').text()
-            
-                json['name']=name
-                json['description']=desc
-                json['price']=price
-                json['size']=size
-
-                msg(json)
-                callback && callback(null,json)
-        }else if(url.match("naturesbasket.co.in")){
-            var name= $('.pd_Title').text()
-            var desc=$('#divShortDesc').text()
-            var price=$('.search_PSellingP').text()
-            var image=$('#impProductsImage').attr("src")
-            
-                json['name']=name
-                json['description']=desc
-                json['price']=price
-                json['image']=image
-
-                msg(json)
-                callback && callback(null,json)
-        }else{
-            info();
-            callback && callback("Websites supported for scraping - BigBasket, Grofers and Natures Basket",null)
-        }
+    }else if(!url.match("grofers.com") && !url.match("bigbasket.com") && !url.match("naturesbasket.co.in") ){
         
+        info();
+        callback && callback("Websites supported for scraping - BigBasket, Grofers and Natures Basket",null)
+    }else{
+        try {
+            var options = {
+                headers: {'user-agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.107 Safari/537.36'},
+                method: 'GET'
+            }
+            request(url, options, function(err, response, html){
+              if(!err){
+                var $= cheerio.load(html);
+                var json={}
+                
+                if(url.match("grofers.com")){
+                    var name= $('.pdp-product__name').text()
+                    var desc=$('.product-attribute__attr--description').text()
+                    var price=$('.pdp-product__price--new').text()
+                    var size=$('.product-variant__btn--active').text()
+                      
+                    json['name']=name
+                    json['description']=desc
+                    json['price']=price
+                    json['size']=size
+                    msg(json)
+                    callback && callback(null,json)
+                }else if(url.match("bigbasket.com")){
+                    var name= $('h1').text()
+                    var desc=$('._26MFu').children().eq(1).find('p').text()
+                    var price=$('.IyLvo').text()
+                    var size=$('._3Yybm').text()
+                    
+                        json['name']=name
+                        json['description']=desc
+                        json['price']=price
+                        json['size']=size
+        
+                        msg(json)
+                        callback && callback(null,json)
+                }else if(url.match("naturesbasket.co.in")){
+                    var name= $('.pd_Title').text()
+                    var desc=$('#divShortDesc').text()
+                    var price=$('.search_PSellingP').text()
+                    var image=$('#impProductsImage').attr("src")
+                    
+                        json['name']=name
+                        json['description']=desc
+                        json['price']=price
+                        json['image']=image
+        
+                        msg(json)
+                        callback && callback(null,json)
+                }
+            
+             }else{
+            
+                msg("Oops something went wrong")
+                callback && callback("Oops something went wrong",null)
+            }})
+         
+        }catch(err){
+            msg("Oops something went wrong")
+            callback && callback("Oops something went wrong",null)
+        }
     }
-})}
+
+    
+}
  
 var msg=function(message){
     if (require.main === module) {
@@ -97,12 +112,15 @@ function argsCheck() {
 
 if (require.main === module) {
     argsCheck();
+    
 }
 
 function info() {
+    if (require.main === module) {
     console.log("Websites supported for scraping - BigBasket, Grofers and Natures Basket");
     console.log("Command to run this npm");
     console.log("node app.js -u Your-URL-String OR -url Your-URL-String");
+    }
 }
 
 module.exports = {
